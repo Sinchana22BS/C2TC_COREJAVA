409e523699d6ed6ac0bd12d55ab7cc57e9e57a08
SINCHANA BS <sinchanabs91@gmail.com>
2022-11-15 22:18:34 +0530

    first commit

---
+++ InstanceOfOperator/src/com/cg/InstanceOf/Animal.java
@@ -0,0 +1,13 @@
+package com.cg.InstanceOf;
+class Animal {
+}
+class Dog1 extends Animal
+{
+
+	public static void main(String[] args) {
+		Dog1 d = new Dog1();
+		System.out.println(d instanceof Animal);
+
+	}
+
+}
---
+++ InstanceOfOperator/src/com/cg/InstanceOf/Demo1.java
@@ -0,0 +1,11 @@
+package com.cg.InstanceOf;
+
+class Demo1 {
+
+	public static void main(String[] args) {
+		Demo1 d = null;
+		System.out.println(d instanceof Demo1);
+
+	}
+
+}
