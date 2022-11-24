f485c2d9086a1f50a01b45a5504c03b9e9c936d6
SINCHANA BS <sinchanabs91@gmail.com>
2022-11-24 23:50:29 +0530

    initial commit

---
+++ Collection_set/src/com/cg/collectionset/Main.java
@@ -0,0 +1,18 @@
+package com.cg.collectionset;
+import java.util.Set;
+import java.util.HashSet;
+public class Main {
+
+	public static void main(String[] args) {
+		Set s = new HashSet();
+		s.add("Technology");
+		s.add("Practice");
+		s.add("java");
+		s.add(4.99);
+		s.add(5);
+		System.out.println(s);
+		
+
+	}
+
+}
---
+++ Collection_set/src/com/cg/collectionset/Set.java
@@ -0,0 +1,13 @@
+package com.cg.collectionset;
+import java.util.LinkedHashSet;
+public class Set {
+	public static void main(String args[])
+	{
+		LinkedHashSet<String> linkedset = new LinkedHashSet<String>();
+		linkedset.add("create");
+		linkedset.add("Own");
+		linkedset.add("Program");
+		System.out.println(linkedset);
+	}
+
+}
